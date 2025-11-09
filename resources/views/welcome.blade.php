60ff1b761c2a711460207d1d4d9ace6ca2b6a202
Polanch <ampolanch@gmail.com>
2025-11-09 08:48:29 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Polanch</title>
+            @vite(['resources/css/style.css','resources/js/script.js'])
+</head>
+<body>
+    <section class="part_one">
+        <div class="line">
+            <h1>hello</h1>
+        </div>
+    </section>
+</body>
+</html>
